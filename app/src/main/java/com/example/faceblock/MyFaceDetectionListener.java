63d8d840834242e3122e577dbe8b1cfd254cf795
Dave Nugent <dave.nuge22@gmail.com>
2019-04-05 15:36:15 -0500

    actually adding files

---
+++ app/src/main/java/com/example/faceblock/MyFaceDetectionListener.java
@@ -0,0 +1,19 @@
+//package com.example.faceblock;
+//
+//import android.hardware.Camera;
+//import android.hardware.camera2.params.Face;
+//import android.util.Log;
+//
+//class MyFaceDetectionListener implements Camera.FaceDetectionListener {
+//
+//    @Override
+//    public void onFaceDetection(Face[] faces, Camera camera) {
+//        if (faces.length > 0){
+//            Log.d("FaceDetection", "face detected: "+ faces.length +
+//                    " Face 1 Location X: " + faces[0].rect.centerX() +
+//                    "Y: " + faces[0].rect.centerY() );
+//        }
+//    }
+//
+//}
+
